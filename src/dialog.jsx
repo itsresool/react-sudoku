c95161a88967303e5c9799534ad1f4c9c4f51f5c
resool <15248621+itsresool@users.noreply.github.com>
2018-10-05 22:42:45 +0200

    display time it took to complete sudoku

--- src/dialog.jsx
+++ src/dialog.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
 const StyledDialog = styled.div`
   height: 300px;
@@ -37,11 +37,11 @@ const StyledButton = styled.button`
   }
 `;
 const Main = styled.div`
-  display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+  display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
   justify-content: center;
   position: absolute;
   align-items: center;
-  pointer-events: ${({ isOpen }) => (isOpen ? "" : "none")};
+  pointer-events: ${({ isOpen }) => (isOpen ? '' : 'none')};
   top: 0;
   left: 0;
   width: 100%;
@@ -55,7 +55,13 @@ class Dialog extends Component {
   state = { isOpen: this.props.isOpen };
 
   render() {
-    const { header, message, isOpen, stateManager } = this.props;
+    const {
+      header,
+      message,
+      completionTimeMessage,
+      isOpen,
+      stateManager
+    } = this.props;
 
     return (
       <Main isOpen={isOpen}>
@@ -63,9 +69,10 @@ class Dialog extends Component {
           <StyledDialog>
             <Title>{header}</Title>
             <Message>{message}</Message>
+            <Message>{completionTimeMessage}</Message>
 
             <StyledButton onClick={stateManager}>
-              {"🔥 Wow I am awesome 🔥"}
+              {'🔥 Wow I am awesome 🔥'}
             </StyledButton>
           </StyledDialog>
         )}
